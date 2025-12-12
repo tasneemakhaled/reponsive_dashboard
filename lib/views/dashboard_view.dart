56b2a9812878bad645aab2a6b4813d9bcbca5b1c
tasneemkhaled <khaledtasneem402@gmail.com>
2025-12-12 19:09:30 +0200

    adding app styles and generate all assets

---
+++ lib/views/dashboard_view.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dashboard/widgets/adaptive_layout_widget.dart';
+import 'package:responsive_dashboard/widgets/desktop_layout.dart';
+import 'package:responsive_dashboard/widgets/mobile_layout.dart';
+import 'package:responsive_dashboard/widgets/tablet_layout.dart';
+
+class DashboardView extends StatelessWidget {
+  const DashboardView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AdaptiveLayoutWidget(
+        mobileLayout: (context) => MobileLayout(),
+        tabletLayout: (context) => TabletLayout(),
+        desktopLayout: (context) => DesktopLayout(),
+      ),
+    );
+  }
+}
